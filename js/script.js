50d6436b41f5e978e7f5b59639b3bb2bc03b2f6a
Pablo Toledo Cordovil <pablotoledoemail@gmail.com>
2023-08-04 12:06:29 -0300

    mais opções de calculo

--- js/script.js
+++ js/script.js
@@ -2,10 +2,29 @@ var input1 = document.querySelector('#input1');
 var botao = document.querySelector('#botaoinput');
 var input2 = document.querySelector('#input2');
 var resultado = document.querySelector('#resultado');
+var botao2 = document.querySelector('#botaoinput2')
+var botao3 = document.querySelector('#botaoinput3')
 
 botao.addEventListener('click', () =>{
     var calculo = parseInt(input1.value) + parseInt(input2.value);
     resultado.innerHTML = calculo;
 
 })
+
+botao2.addEventListener('click', () => {
+
+    var calculo2 = parseInt(input1.value) / parseInt(input2.value);
+    resultado.innerHTML = calculo2;
+
+})
+
+botao3.addEventListener('click', () => {
+
+    var calculo3 = parseInt(input1.value) * parseInt(input2.value);
+    resultado.innerHTML = calculo3;
+})
+
+
+
+
 
